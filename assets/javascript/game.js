39f1ee0d79c90e9f196584b4b9aaaef197e7d9b4
Winterzune <winterzune@gmail.com>
2018-09-11 01:25:07 -0400

    nope

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,33 @@
+
+//created array, idea is if user guesses all the letters in the array to form the chosen word (carbon), then they win.
 var altered = ["c", "a", "r", "b", "o", "n"];
 
+
+//starting the counters at 0
 var wins = 0;
 var loss = 0;
-var userguess = 0;
+var userguess;
 
 
 
-var listenForKeys = function () {
-document.onkeypress = handleKeyPress
-};
+//link to make the value show in the html tags
+var wincounter = document.getElementById("wincounter");
 
-var handleKeyPress = function () {
+//listening for keypresses
+document.onkeypress = function(event) {
+var userguess = event.key
 
-};
-
-var wincount = document.getElementById("wincounter");
-var losscount = document.getElementById("losscounter");
-
-document.onkeyup =  function(event) {
-      var userguess = event.key;
-
-
+//conditions set broad for now, so if user guesses c or a or ....
 if ((userguess === "c") || (userguess === "a") || (userguess === "r") || (userguess === "b") || (userguess === "o") || (userguess === "n")) {
 
-     return wins ++;
-}
-else if (altered.indexOf < -1) {
-      
-     return    loss++ ;
-}}
+ // I'm confused with syntax, as to how to say what I want with the program. I understand I'm adding here, but I'm not sure if all I need is to say return or what.
+    
+ return (wins++);  
+    }
+
+};
+
+//would do the same for the losing counter
+//var losscount = document.getElementById("losscounter");
+
+//not sure how to store the incorrect keypresses and display them into the website.
